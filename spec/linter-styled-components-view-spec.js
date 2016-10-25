3cc1871789617276a51aafffe4227adb4f5a2419
Nazim Hajidin <nhajidin@gmail.com>
2016-10-25 17:57:34 -0400

    Add generated files

---
+++ spec/linter-styled-components-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LinterStyledComponentsView from '../lib/linter-styled-components-view';
+
+describe('LinterStyledComponentsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
